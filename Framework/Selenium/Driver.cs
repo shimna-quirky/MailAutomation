68230b82a6c7b76d77371b508f1d27655d0ce435
shimna-quirky <shimna.quirky@gmail.com>
2021-08-17 21:37:57 +1000

    CodeRefactor

--- Framework/Selenium/Driver.cs
+++ Framework/Selenium/Driver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -9,9 +10,26 @@ namespace Framework.Selenium
     {
         [ThreadStatic]
         private static IWebDriver _driver;
+             
         public static void Init()
         {
-            _driver= new ChromeDriver(Path.GetFullPath(@"../../../../" + "_drivers"));
+            var browser = FW.FConfig.Driver.Browser;
+            switch(browser)
+            {
+                case Browsers.ChromeBrowser:
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("--start-maximized");
+                    chromeOptions.AddArgument("--disable-translate");
+                    chromeOptions.AddArgument("--disable-popup-blocking");
+                    _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),chromeOptions);
+                    var drpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    break;
+                default:
+                    throw new Exception("");
+
+            }
+            
+            
         }
         public static IWebDriver Current => _driver ?? throw new NullReferenceException("_driver is null");
     }
